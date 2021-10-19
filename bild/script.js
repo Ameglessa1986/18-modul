3848182af01c3dd1b491cf7c4e613b86162e28d9
Alekseeva Anastasiya <faya-1023@mail.ru>
2021-10-19 20:39:19 +0300

    complite

--- bild/script.js
+++
@@ -1,6 +0,0 @@
-(()=>{'use strict';var e=function(e,n)
-{var r=document.createElement('ul');
-  r.className=void 0;var t='';
-  return['Главная','О себе','Контакты']
-    .forEach((function(e){t+='<li>'+e+'</li>';})),
-  r.innerHTML=t,r;}();document.body.appendChild(e);})();
